6d5e689c42279b9b6eca58a97e2c46f293cee7b1
Aituglo <ckhouani@live.fr>
2017-06-09 12:02:12 +0200

    Add Update

--- rpi-update.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-
-curl -SLs https://raw.githubusercontent.com/OnyxProject/onyx-build-maker/master/update-onyx.sh | bash
--- update-onyx.sh
+++ update-onyx.sh
@@ -1,12 +1,35 @@
 #!/bin/bash
 
-ONYX_VERSION=1.0.0
+function validate_url(){
+  if [[ `wget -S --spider $1  2>&1 | grep 'HTTP/1.1 200 OK'` ]]; then echo "true"; fi
+}
 
+ONYX_VERSION=$(curl -s 'https://raw.githubusercontent.com/OnyxProject/Onyx/master/version.json' | jq -r '.version')
 ONYX_FOLDER="/home/pi/Onyx"
+TMP_DB="/tmp/onyx_db"
+TMP_DATA="/tmp/onyx_data"
+ONYX_URL = http://download.onyxlabs.fr/onyx-build/onyx-v$ONYX_VERSION-Linux-armv6l.tar.gz
 
-wget http://download.onyxlabs.fr/onyx-build/onyx-v$ONYX_VERSION-Linux-armv6l.tar.gz
+if `validate_url $ONYX_URL >/dev/null`;
+then
+  wget $ONYX_URL
+else
+  echo "FILE NOT EXIST"
+  echo "EXIT"
+  exit 1
+fi
 
 # stop onyx
+sudo onyx stop
+
+rm -rf $TMP_DB
+rm -rf $TMP_DATA
+
+mkdir $TMP_DB
+mkdir $TMP_DATA
+
+cp -ar $ONYX_FOLDER/onyx/db/. $TMP_DB
+cp -ar $ONYX_FOLDER/onyx/data/. $TMP_DATA
 
 # Delete current onyx install
 rm -rf $ONYX_FOLDER
@@ -14,7 +37,9 @@ rm -rf $ONYX_FOLDER
 #  install onyx
 tar zxvf onyx-v$ONYX_VERSION-Linux-armv6l.tar.gz
 
-# init
-cd $ONYX_FOLDER
+cp -ar $TMP_DB/. $ONYX_FOLDER/onyx/db
+mkdir $ONYX_FOLDER/onyx/data
+cp -ar $TMP_DATA/. $ONYX_FOLDER/onyx/data
 
 # restart Onyx
+sudo onyx start
